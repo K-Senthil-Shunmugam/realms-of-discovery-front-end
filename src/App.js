7ba6d6dda69459190724d30a44e61981390bde5a
Administrator <senthilshunmugam2003@gmail.com>
2024-12-22 06:40:34 +0000

    Update file App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route
           path="/"
-          element={cookies.accountDetails.accountid === undefined ? <Navigate to="/login" /> : <Home />}
+          element={cookies.accountDetails=== undefined || cookies.accountDetails === 'undefined' ? <Navigate to="/login" /> : <Home />}
         />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
