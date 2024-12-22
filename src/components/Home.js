644df1664de45e673e1fee1a580cea786b333369
Administrator <senthilshunmugam2003@gmail.com>
2024-12-22 03:40:49 +0000

    Update file Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ import './Home.css';
 
 const Home = () => {
   const navigate = useNavigate();
-  const [cookies, removeCookie] = useCookies(['accountID']);
+  const [cookies, setCookie, removeCookie] = useCookies(['accountID', 'username']);
   const username = cookies.username;
 
   const handleLogout = async () => {
@@ -16,9 +16,9 @@ const Home = () => {
         accountID: cookies.accountID, // Include the accountID in the request body
       });
 
-      // Remove the accountID cookie on the client-side
-      removeCookie('accountID', { path: '/' });
-      removeCookie('username', { path: '/' });
+      // Set the cookies to undefined and set their expiration to the past
+      setCookie('accountID', undefined, { path: '/', expires: new Date(0) });
+      setCookie('username', undefined, { path: '/', expires: new Date(0) });
 
       // Redirect to the login page
       navigate('/login');
@@ -31,7 +31,9 @@ const Home = () => {
     <div className="home-page-body">
       <div className="home-container">
         <div className="home-box">
-          <h1 className="home-h1-welcome-text">Welcome back {username}!</h1>
+          <h1 className="home-h1-welcome-text">
+            Welcome back {username ? username : 'Guest'}!
+          </h1>
           <p className="home-paragraph">You are logged in!</p>
           <button onClick={handleLogout} className="home-logout-btn">Logout</button>
         </div>
